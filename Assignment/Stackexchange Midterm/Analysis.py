41275b9c2906baa9ee78fe1d70ed6e0329d263bf
Mohan <I59239@verisk.com>
2016-11-01 14:42:56 -0400

    Midtermv05

--- Assignment/Stackexchange Midterm/Analysis.py
+++ Assignment/Stackexchange Midterm/Analysis.py
@@ -1,8 +1,78 @@
 import os
 import pandas as pd
+import datetime
+#import plotly.plotly as py
+#import plotly.graph_objs as go
 #import argparse
 u=pd.read_csv('processed_users.csv')
 q=pd.read_csv('processed_questions.csv')
 a=pd.read_csv('processed_answers.csv')
-print(p)
-#result = pd.merge(q, u, on='key')
+
+q_u = pd.merge(q, u, on='user_id')
+a_u = pd.merge(a, u, on='user_id')
+#q_a_u = pd.merge(q_u,a_u, on='answer_id')
+
+#print(t)
+
+
+#Analysis-1: Top 10 Questions deemed difficult -> Analyze down
+q_u = q_u[(q_u.is_answered==True)]
+#print(q_u.head(5))
+#print(a_u.head(5))-'answer_creation_date'
+anal1_q_a_u=pd.merge(q_u, a_u, on='question_id')
+anal1_q_a_u=anal1_q_a_u[(anal1_q_a_u.is_accepted==True)]
+anal1_q_a_u['question_creation_date']=pd.to_datetime(anal1_q_a_u['question_creation_date'],infer_datetime_format=True)
+anal1_q_a_u['answer_creation_date']=pd.to_datetime(anal1_q_a_u['answer_creation_date'],infer_datetime_format=True)
+anal1_q_a_u['date_diff']=anal1_q_a_u['answer_creation_date']-anal1_q_a_u['question_creation_date']
+result_1 = anal1_q_a_u.sort_values(by=[ 'date_diff','view_count'], ascending=[0, 0])
+result_1=result_1[['question_id','title','date_diff','view_count']]
+result_1=result_1.head(10)
+result_1.to_csv('Analysis_1_Questions_deemed_difficult.csv')
+
+
+#Analysis-2: Top 10 Experts in Python and Pandas (Highest badges plus most answers) -> Unanswered Questions related to their experise can be forwarded to them.
+
+#one=df1.groupby('searchterm').mean()  df3.groupby(['searchterm','location'])['location'].count()
+u2=u.copy()
+u2=u2[['display_name','gold_badge_counts','silver_badge_counts','bronze_badge_counts','user_id']]
+u2['total_badge_count']=u2['gold_badge_counts']+u2['silver_badge_counts']+u2['bronze_badge_counts']
+#print(u.head(10))
+ans2=a[['answer_id','user_id']]
+#print(a.head(10))
+a_u=pd.merge(ans2,u2,on='user_id')
+#print(a_u.head(10))
+result_2=a_u.groupby(['user_id','display_name','total_badge_count'])['answer_id'].count()
+result_2 = result_2.sort_index(level="total_badge_count", ascending=False)
+result_2=result_2.head(10)
+result_2.to_csv('Analysis_2_Experts.csv')
+
+
+#Analysis-3: Python question trend over year-month combination -> Trend followed by particular technology in the market place
+
+q3=q.copy()
+q3['question_creation_date']=pd.to_datetime(q3['question_creation_date'],infer_datetime_format=True)
+q3['YearMonth'] = q3['question_creation_date'].map(lambda x: 1000*x.year + x.month)
+#print(len(q3))
+q3=q3.groupby(['YearMonth'])['YearMonth'].count()
+#print(len(q3))
+q3.to_csv('Analysis_3_YearMonth_Question_Distribution.csv')
+
+
+#Analysis-4: Top 10 Most frequently viewed questions in Python -> Can be pulled every week
+
+q4=q.copy()
+q4['question_creation_date']=pd.to_datetime(q4['question_creation_date'],infer_datetime_format=True)
+q4['Year'] = q4['question_creation_date'].map(lambda x: x.year)
+q4=q4[['Year','title','question_id','view_count']]
+#print(q4.nlargest(10, 'view_count'))
+q4.to_csv('Analysis_4_Most_freq_viewed_quest.csv')
+#print(result_2.sort_values(by='[total_badge_count','count'],ascending=[0,0]))
+
+
+#Analysis-5: Top 10 users with greatest reputation:
+u5=u.copy()
+u5=u5[['user_id','reputation']]
+u5a=u5.nlargest(10, 'reputation')
+u5b=u5.nsmallest(10, 'reputation')
+u5a.to_csv('Analysis_5a_top10_users_highrep.csv')
+u5b.to_csv('Analysis_5b_top10_users_lowrep.csv')
--- Assignment/Stackexchange Midterm/processcsv.py
+++ Assignment/Stackexchange Midterm/processcsv.py
@@ -12,7 +12,7 @@ for filename in os.listdir(qloc):
             if "user_id" in data["owner"].keys():
                 qrow.append(str(data.get("is_answered",'none'))+','+str(data.get("view_count",'none'))+','+
                        str(data.get("answer_count",'none'))+','+str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get("creation_date",'none'))))+','+str(data.get("score",'none'))+','+str(data.get("accepted_answer_id",'none'))
-                       +','+str(data["owner"]["user_id"])+','+data.get("title",'none')+','+str(data.get("question_id",'none'))+'\n')
+                       +','+str(data["owner"]["user_id"])+','+data.get("title",'none').replace(',','')+','+str(data.get("question_id",'none'))+'\n')
 with open('processed_questions.csv','w') as f:
     for i in range(0,len(qrow)):
         f.write(qrow[i].encode('utf-8'))
@@ -27,7 +27,7 @@ for filename in os.listdir(aloc):
         with open(aloc+'/'+filename) as data_file:
             data = json.load(data_file)
             if "user_id" in data["owner"].keys():
-                arow.append(filename[:filename.find('_')-1]+','+str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get("creation_date",'none'))))+','
+                arow.append(filename[:filename.find('_')]+','+str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get("creation_date",'none'))))+','
                             +str(data.get("score",'none'))+','+str(data["owner"]["user_id"])+','+str(data.get("is_accepted",'none'))+','+filename[filename.find('_')+1:-5]+'\n')
 with open('processed_answers.csv','w') as f:
     for i in range(0,len(arow)):
@@ -38,7 +38,7 @@ with open('processed_answers.csv','w') as f:
 urow=[]
 uloc='topics/users'
 dict_badge={}
-urow.append('is_employee,user_id,creation_date,last_access_date,reputation,gold_badge_counts,silver_badge_counts,bronze_badge_counts\n')
+urow.append('is_employee,user_id,creation_date,last_access_date,reputation,gold_badge_counts,silver_badge_counts,bronze_badge_counts,display_name\n')
 for filename in os.listdir(uloc):
     if filename.endswith(".json"):
         with open(uloc+'/'+filename) as data_file:
@@ -49,7 +49,7 @@ for filename in os.listdir(uloc):
             #if "gold" in data["owner"].keys():
             urow.append(str(data.get("is_employee",'none'))+','+filename[:-5]+','+str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get("creation_date",'none'))))+','+
                         str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data.get("last_access_date",'none'))))+','+str(data.get("reputation",'none'))+','+
-                        str(data["badge_counts"]["gold"])+','+str(data["badge_counts"]["silver"])+','+str(data["badge_counts"]["bronze"])+'\n')
+                        str(data["badge_counts"]["gold"])+','+str(data["badge_counts"]["silver"])+','+str(data["badge_counts"]["bronze"])+','+data.get("display_name",'none')+'\n')
 with open('processed_users.csv','w') as f:
     for i in range(0,len(urow)):
         f.write(urow[i].encode('utf-8'))
